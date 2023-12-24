3e4fc2db606605cc287fe6def70210b563479e14
Gabiroba <gabrielcardosogirarde@gmail.com>
2023-12-24 15:43:54 -0300

    alteração no README

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,7 +1,35 @@
-﻿
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-using Consumer;
+const string HOST_NAME = "localhost";
+const string EXCHANGE_NAME = "rabbit_mq";
 
-ConsumerClass c = new();
+const string QUEUE_NAME = "lojinha.Gabiroba";
 
-c.ReceiveAsync(value => Console.WriteLine(value));
+const string QUEUE_KEY = "lojinha.Gabiroba.Key";
+
+var factory = new ConnectionFactory
+{
+    HostName = HOST_NAME
+};
+
+using var connection = factory.CreateConnection(); // Obtendo a conexão
+
+var channel = connection.CreateModel();
+
+channel.QueueBind(exchange: EXCHANGE_NAME, queue: QUEUE_NAME, routingKey: QUEUE_KEY);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, arg) => 
+{
+    Console.WriteLine(Encoding.UTF8.GetString(arg.Body.Span));
+};
+
+
+channel.BasicConsume(
+    queue: QUEUE_NAME,
+    autoAck: true,
+    consumer: consumer
+);
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,5 +1,44 @@
-﻿using Publisher;
+﻿using System.Text;
+using RabbitMQ.Client;
 
-PublisherClass p = new();
+const string HOST_NAME = "localhost";
+const string EXCHANGE_NAME = "RABBITMQ";
 
-p.Send("Hello World");
+const string QUEUE_NAME = "lojinhaGabiroba";
+
+const string QUEUE_KEY = "lojinhaGabirobaKey";
+
+var factory = new ConnectionFactory
+{
+    HostName = HOST_NAME
+};
+
+using var connection = factory.CreateConnection(); // Obtendo a conexão
+
+using var channel = connection.CreateModel(); // Criando o channel
+
+channel.ExchangeDeclare(  // Criando um Exchange
+    exchange: EXCHANGE_NAME,
+    type: ExchangeType.Direct
+);
+
+var queue = channel.QueueDeclare( // Criando uma Queue
+    queue: QUEUE_NAME,
+    durable: false,
+    autoDelete: false,
+    arguments: null
+); 
+
+channel.QueueBind(  // Ligando a Queue na exchange
+    exchange: EXCHANGE_NAME,
+    queue: QUEUE_NAME,
+    routingKey: QUEUE_KEY
+);
+
+channel.BasicPublish( // Publicando a mensagem na Queue lojinha.Gabiroba através da exchange lojinha.Gabiroba.Key
+    exchange: EXCHANGE_NAME,
+    routingKey: string.Empty,
+    body: Encoding.UTF8.GetBytes("Hello World, I'm here!")
+);
+
+Console.WriteLine("Queue created with success");
